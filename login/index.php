452939c2c33d08acf190d42d72a1013b66f588e0
erhowell <erhowell15@gmail.com>
2018-11-17 03:12:13 -0700

    change

--- login/index.php
+++ login/index.php
@@ -19,7 +19,6 @@
       </button>
     </div>
     <div id="main-content">
-<<<<<<< HEAD
       <form action="login_connect.php" method="post">
         <div class="tab active" id="login">
           <h1>Log In To Your Account</h1>
@@ -31,7 +30,7 @@
             <label>Password</label>
             <input class="pw" type="text" name="password"/>
           </div>
-          <button class="submit" onclick="submitLoginData()">Submit</button>
+         <input type="submit" value="Login">
         </div>
       </form>
       <form action="signup_connect.php" method="post">
@@ -61,57 +60,9 @@
             <input type="radio" name="account" class="account-type" value="professional">Professional<br>
             <input type="radio" name="account" class="account-type" value="client">Client<br>
           </div>
-          <button class="submit" onclick="submitSignupData()">Submit</button>
+          <input type="submit" value="Login">
         </div>
       </form>
-=======
-      <div class="tab active" id="login">
-        <h1>Log In To Your Account</h1>
-        
-        <form action="check.php" method="POST">
-          <div class="field">
-          <label>Email</label>
-          <input class="email" type="email" name="email"/>
-        </div>
-        <div class="field">
-          <label>Password</label>
-          <input class="pw" type="text" name="Password"/>
-        </div>
-        <input type="submit" name="submit" value="Submit"> 
-        </form>
-      </div>
-
-
-      <div class="tab" id="signup">
-        <h1>Create Your Account</h1>
-        <div class="field">
-          <label>First Name</label>
-          <input class="fname" type="text"/>
-        </div>
-        <div class="field">
-          <label>Last Name</label>
-          <input class="lname" type="text"/>
-        </div>
-        <div class="field">
-          <label>Email</label>
-          <input class="email" type="email"/>
-        </div>
-        <div class="field">
-          <label>Password</label>
-          <input class="pw" type="text"/>
-        </div>
-        <div class="field">
-          <label>Retype Password</label>
-          <input class="pw" type="text"/>
-        </div>
-        <div>
-          <ul>
-            <li>Professional</li>
-            <li>Client</li>
-          </ul>
-        </div>
-      </div>
->>>>>>> fb0f5af1d2365ee6e2d26d0114a7b907a6790bc3
     </div>
   </div>
   <script src="script.js"></script>
--- login/login_connect.php
+++ login/login_connect.php
@@ -7,18 +7,20 @@ $link = mysqli_connect("localhost", "root", "", "therapysite");
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-
-
-//validate login info
-
-
-
-if(mysqli_query($link, $sql)){
-    echo "Records added successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
  
-// Close connection
-mysqli_close($link);
+$email = mysqli_real_escape_string($link, $_REQUEST['email']);
+$password = mysqli_real_escape_string($link, $_REQUEST['Password']);
+// Attempt insert query execution
+echo $email;
+echo $password;
+ $sql = "SELECT * FROM user
+ 		WHERE email='$email' and password='$password'";
+
+ if (mysqli_query($link,$sql)){
+      echo "<script>window.location = '../index.php'</script>";
+  }
+  else{
+  	echo("Error description: " . mysqli_error($link));
+  }
+  mysqli_close($link);
 ?>
