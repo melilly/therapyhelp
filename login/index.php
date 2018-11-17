d02e8e3e9fffaf3d82e790a2ccc2dfe4507a2a19
Melissa Rasmussen <melissa.ann.r@gmail.com>
2018-11-17 03:03:09 -0700

    Merge complete.

--- login/index.php
+++ login/index.php
@@ -19,7 +19,6 @@
       </button>
     </div>
     <div id="main-content">
-<<<<<<< HEAD
       <form action="login_connect.php" method="post">
         <div class="tab active" id="login">
           <h1>Log In To Your Account</h1>
@@ -64,54 +63,6 @@
           <button class="submit" onclick="submitSignupData()">Submit</button>
         </div>
       </form>
-=======
-      <div class="tab active" id="login">
-        <h1>Log In To Your Account</h1>
-        
-        <form action="check.php" method="POST">
-          <div class="field">
-          <label>Email</label>
-          <input class="email" type="email" name="email"/>
-        </div>
-        <div class="field">
-          <label>Password</label>
-          <input class="pw" type="text" name="Password"/>
-        </div>
-        <input type="submit" name="submit" value="Submit"> 
-        </form>
-      </div>
-
-
-      <div class="tab" id="signup">
-        <h1>Create Your Account</h1>
-        <div class="field">
-          <label>First Name</label>
-          <input class="fname" type="text"/>
-        </div>
-        <div class="field">
-          <label>Last Name</label>
-          <input class="lname" type="text"/>
-        </div>
-        <div class="field">
-          <label>Email</label>
-          <input class="email" type="email"/>
-        </div>
-        <div class="field">
-          <label>Password</label>
-          <input class="pw" type="text"/>
-        </div>
-        <div class="field">
-          <label>Retype Password</label>
-          <input class="pw" type="text"/>
-        </div>
-        <div>
-          <ul>
-            <li>Professional</li>
-            <li>Client</li>
-          </ul>
-        </div>
-      </div>
->>>>>>> fb0f5af1d2365ee6e2d26d0114a7b907a6790bc3
     </div>
   </div>
   <script src="script.js"></script>
