cfa335f4c9b4579c445a8b7d772563a599511604
erhowell <erhowell15@gmail.com>
2018-11-16 20:54:19 -0700

    update

--- SearchPage/index.php
+++ SearchPage/index.php
@@ -6,6 +6,28 @@
 </head>
 
 <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="#">Therapist</a>
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="profile/profile.php">Your Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="sessions/sessions.php">Your sessions</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Connect with a Professional</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
 	<h1>Search</h1>
 	<div class="search">
 		<div class="name">
--- profile/profile.php
+++ profile/profile.php
@@ -21,7 +21,7 @@
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="profile/profile.html">Your Profile</a>
+            <a class="nav-link" href="profile/profile.php">Your Profile</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="sessions/sessions.php">Your sessions</a>
