ad1ab20f32f1e2efdaa07c8305f26387314443f3
erhowell <erhowell15@gmail.com>
2018-11-17 03:13:08 -0700

    Revert "Update SearchPage/index.php"
    This reverts commit fb0f5af1d2365ee6e2d26d0114a7b907a6790bc3.

--- SearchPage/index.php
+++ SearchPage/index.php
@@ -11,28 +11,6 @@
 </head>
 
 <body>
-  <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "therapysite";
-
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  // Check connection
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  } 
-
-  $sql = "Select * FROM specialties";
-    if ($result=mysqli_query($conn,$sql))
-    {
-  // Fetch one and one row
-  // Free result set
-  }
-
-  $conn->close();
-  ?> 
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -83,20 +61,45 @@
           <input type="text" placeholder="Maximum Hourly rate.." name="Hourly rate maximum">
      </div>
       <p> Specialties</p>
-      <ul>
-      <?php
-        while ($row=mysqli_fetch_row($result))
-      {
-        echo "<li style='list-style-type: none'><input type='radio' name='specialties' value='$row[1]'>$row[1]</li>";
-      }
-      ?>
-    </ul>
+      <div>
+           <label class="checkbox"> 
+            <input type="checkbox" checked="checked">
+            Marriage and family counseling.
+           </label>
+      </div>
+      <div>
+           <label class="checkbox">
+            <input type="checkbox" checked="checked">
+            Guidance and career counseling.
+           </label>
+      </div>
+      <div>
+             <label class="checkbox">
+            <input type="checkbox" checked="checked">
+            Rehabilitation counseling.
+           </label>
+      </div>
+      <div>
+           <label class="checkbox">
+            <input type="checkbox" checked="checked">
+            Mental health counseling
+           </label>
+      </div>
+      <div>
+           <label class="checkbox">
+            <input type="checkbox" checked="checked">
+            Substance abuse counseling
+           </label>
+      </div>
+      <div>
+           <label class="checkbox">
+            <input type="checkbox" checked="checked">
+            Educational Counseling.
+           </label>
+      </div>
       <button>Search</button>
     </div>
 	</div>
-  <div id="results">
-    
-  </div>
        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
