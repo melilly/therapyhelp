a15a1902b2f598b1e07e243de67f6b34d1499e84
erhowell <erhowell15@gmail.com>
2018-11-17 03:45:34 -0700

    login try

--- login/insert.php
+++ login/insert.php
@@ -13,14 +13,17 @@ $first_name = mysqli_real_escape_string($link, $_REQUEST['first_name']);
 $last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
 $email = mysqli_real_escape_string($link, $_REQUEST['email']);
 $password = mysqli_real_escape_string($link, $_REQUEST['pw']);
+$isClient = mysqli_real_escape_string($link, $_REQUEST['radio']);
 // Attempt insert query execution
  $sql = "INSERT INTO `user`( `fName`, `lName`, `email`, `password`, `isVerified`, `isClient`)
-        VALUES ()"
+        VALUES ($fName, $lName, $email, $pw, $radio)";
 
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
+    echo "<h3>Sucess</h3>";
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    echo "<h3>Fail</h3>";
 }
  
 // Close connection
