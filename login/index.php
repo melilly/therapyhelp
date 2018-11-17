b68a8048b83a3e695e2bbbd67e94a91dba924133
Melissa Rasmussen <melissa.ann.r@gmail.com>
2018-11-17 11:25:10 -0700

    remove bootstrap

--- login/index.php
+++ login/index.php
@@ -5,8 +5,6 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width">
 	<title>Login to TherapyHelp</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
   <link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 
@@ -18,44 +16,44 @@
         <button class="top" id="signup-header" onclick="openTab(event, 'signup')">
           Sign Up
         </button>
-      <!--form action="insert.php" method="post"-->
+      <form action="insert.php" method="post">
         <div class="tab active" id="login">
           <h1>Log In To Your Account</h1>
           <div class="field">
-            <input class="email" type="email" placeholder="Email" onkeyup="this.setAttribute('value', this.value);"/>
+            <input class="email" type="email" placeholder="Email" onkeyup="this.setAttribute('value', this.value);" name="email"/>
           </div>
           <div class="field">
-            <input class="pw" type="text" placeholder="Password" onkeyup="this.setAttribute('value', this.value);"/>
+            <input class="pw" type="text" placeholder="Password" onkeyup="this.setAttribute('value', this.value);" name="pw"/>
           </div>
           <button class="submit" onclick="submitLoginData()">Submit</button>
         </div>
-      <!--/form-->
-      <!--form action="insert.php" method="post"-->
+      </form>
+      <form action="insert.php" method="post">
         <div class="tab" id="signup">
           <h1>Create Your Account</h1>
           <div class="field">
-            <input class="fname" type="text" placeholder="First Name" onkeyup="this.setAttribute('value', this.value);"/>
+            <input class="fname" name="first_name" type="text" placeholder="First Name" onkeyup="this.setAttribute('value', this.value);"/>
           </div>
           <div class="field">
-            <input class="lname" type="text" placeholder="Last Name" onkeyup="this.setAttribute('value', this.value);"/>
+            <input class="lname" name="last_name" type="text" placeholder="Last Name" onkeyup="this.setAttribute('value', this.value);"/>
           </div>
           <div class="field">
-            <input class="email" type="email" placeholder="Email" onkeyup="this.setAttribute('value', this.value);"/>
+            <input class="email" name="email" type="email" placeholder="Email" onkeyup="this.setAttribute('value', this.value);"/>
           </div>
           <div class="field">
-            <input class="pw" type="text" placeholder="Password" onkeyup="this.setAttribute('value', this.value);"/>
+            <input class="pw" name="pw" type="text" placeholder="Password" onkeyup="this.setAttribute('value', this.value);"/>
           </div>
           <div class="field">
-            <input class="pw" type="text" placeholder="Retype Password" onkeyup="this.setAttribute('value', this.value);"/>
+            <input class="pw" name="pw_again" type="text" placeholder="Retype Password" onkeyup="this.setAttribute('value', this.value);"/>
           </div>
           <div class="radio-section">
-            <input type="radio" name="account" class="account-type" value="professional">Professional<br>
-            <input type="radio" name="account" class="account-type" value="client">Client<br>
+            <input type="radio" name="type" class="account-type" value="professional">Professional<br>
+            <input type="radio" name="type" class="account-type" value="client">Client<br>
           </div>
           <button class="submit" onclick="submitSignupData()">Submit</button>
         </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      </form>
+  </div>
   <script src="script.js"></script>
   </body>
 
