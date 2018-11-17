42e0b86d0623f674853b022ee304b3852d31397c
erhowell <erhowell15@gmail.com>
2018-11-16 23:50:08 -0700

    Update profile/profile.php

--- profile/profile.php
+++ profile/profile.php
@@ -10,6 +10,40 @@
 	<link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
+  <?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "therapysite";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  } 
+
+  $sql = "SELECT p.user_id, p.location, p.school, p.bio, u.fName, u.lName 
+          FROM professional p
+          JOIN user u ON p.user_id=u.id
+          WHERE p.user_id =4
+          ";
+    if ($result=mysqli_query($conn,$sql))
+    {
+  // Fetch one and one row
+      while ($row=mysqli_fetch_row($result))
+      {
+        $location = $row[1];
+        $school = $row[2];
+        $bio = $row[3];
+        $name = $row[4]. " ". $row[5];
+      }
+  // Free result set
+  mysqli_free_result($result);
+  }
+
+  $conn->close();
+  ?> 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -39,13 +73,13 @@
     <div class="profile">
         <h4 class="profileHeading"><span>Professional</span></h4><hr/>
 		<button type="button" class="btn btn-outline-dark"><a href="profile-edit.php">Edit Profile</a></button>
-        <p><span style="color:#9c47e6;font-weight:bold">Name:</span> Dr. John Smith</p>
+        <p><span style="color:#9c47e6;font-weight:bold">Name:</span><?php echo $name?> </p>
         <p><span style="color:#9c47e6;font-weight:bold">Type of Practice:</span> Therapy</p>
         <p><span style="color:#9c47e6;font-weight:bold">Specialties: </span> Anxiety, depression, bipolar</p>
-        <p><span style="color:#9c47e6;font-weight:bold">School:</span> Utah State University</p>
-        <p><span style="color:#9c47e6;font-weight:bold">Biography:</span> Sample bio</p>
+        <p><span style="color:#9c47e6;font-weight:bold">School:</span> <?php echo $school?></p>
+        <p><span style="color:#9c47e6;font-weight:bold">Biography:</span> <?php echo $bio?></p>
         <p><span style="color:#9c47e6;font-weight:bold">Hourly Rate:</span> $75</p>
-        <p><span style="color:#9c47e6;font-weight:bold">Location:</span> Logan, Utah</p><hr/>
+        <p><span style="color:#9c47e6;font-weight:bold">Location:</span> <?php echo $location?></p><hr/>
         </div>
         <div class="profile">
         <h4 class="profileHeading"><span>Client</span></h4><hr/>
@@ -56,7 +90,7 @@
     </div>   
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
